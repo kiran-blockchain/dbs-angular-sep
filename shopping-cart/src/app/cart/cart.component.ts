53d3bb66b508557a7de977968728e20efe2cee3e
crypto <crypto@cryptos-MBP.Dlink>
2021-09-02 16:40:11 +0530

    added code

--- shopping-cart/src/app/cart/cart.component.ts
+++ shopping-cart/src/app/cart/cart.component.ts
@@ -10,12 +10,17 @@ import { DataService } from '../services/data.service';
 export class CartComponent implements OnInit {
   products:any;
   total=0;
-  constructor(private dataSvc:DataService) { 
+  constructor(public dataSvc:DataService) { 
     this.products = this.dataSvc.getCartItems();
     this.total = this.dataSvc.getCartTotal();
   }
 
   ngOnInit(): void {
   }
-
+  addToCart(item:any){
+    this.dataSvc.addToCart(item)
+  }
+  removeFromCart(item:any){
+    this.dataSvc.removeFromCart(item);
+  }
 }
